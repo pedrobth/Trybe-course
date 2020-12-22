f6ca69b745b4b0c7d4227172af8ebfd14813e8dc
Pedro Risso <pmerchiorattor@gmail.com>
2020-12-22 17:20:37 -0300

    fix: handle promise rejection, wait cart update

--- src/__tests__/requirement8.test.js
+++ src/__tests__/requirement8.test.js
@@ -4,13 +4,29 @@ import App from '../App';
 import * as api from '../services/api';
 import mockedCategoriesResult from '../__mocks__/categories';
 import mockedQueryResult from '../__mocks__/query';
+import mockedIdResult from '../__mocks__/id';
 
 jest.mock('../services/api');
 api.getCategories.mockImplementation(
-  () => Promise.resolve(mockedCategoriesResult),
+  () => new Promise((resolve, reject) => {
+    mockedCategoriesResult
+      ? resolve(mockedCategoriesResult)
+      : reject(error)
+  }),
 );
 api.getProductsFromCategoryAndQuery.mockImplementation(
-  () => Promise.resolve(mockedQueryResult),
+  () => new Promise((resolve, reject) => {
+    mockedQueryResult
+      ? resolve(mockedQueryResult)
+      : reject(error)
+  }),
+);
+api.getProductsFromId.mockImplementation(
+  () => new Promise((resolve, reject) => {
+    mockedIdResult
+      ? resolve(mockedIdResult)
+      : reject(error)
+  })
 );
 
 describe('Adicionar produtos a partir da tela de listagem de produtos', () => {
@@ -20,11 +36,12 @@ describe('Adicionar produtos a partir da tela de listagem de produtos', () => {
     fireEvent.click(screen.getAllByTestId('category')[0]);
     await waitFor(() => expect(api.getProductsFromCategoryAndQuery).toHaveBeenCalled());
     fireEvent.click(screen.getAllByTestId('product-add-to-cart')[0]);
+    await waitFor(() => expect(screen.getByTestId('shopping-cart-size')).toHaveTextContent(1));
     fireEvent.click(screen.getByTestId('shopping-cart-button'));
     await waitFor(() => expect(screen.getAllByTestId('shopping-cart-product-name')));
-    expect(screen.getByTestId('shopping-cart-product-name')).toHaveTextContent(
+    await waitFor(() => expect(screen.getByTestId('shopping-cart-product-name')).toHaveTextContent(
       mockedQueryResult.results[0].title,
-    );
+    ));
     expect(screen.getByTestId('shopping-cart-product-quantity')).toHaveTextContent('1');
   });
 });
